5219fb9f25f9a4847a7937ee9aad2f877e9b35ba
Surya Narayan Jena <30215820+atulsnjena@users.noreply.github.com>
2020-05-19 21:14:00 +0530

    Update p12.py

--- Python Development Task 1/p12.py
+++ Python Development Task 1/p12.py
@@ -1,2 +1,9 @@
 import pandas as pd
-b=pd.read_csv('diamonds.csv')
+
+b = pd.read_csv('diamonds.csv')
+a=len(b.index)
+c=int((len(b.index))*0.75)
+d=pd.read_csv('diamonds.csv' ,nrows=c)
+print(d)
+e=pd.read_csv('diamonds.csv' ,skiprows=c,nrows=(a-c))
+print(e)
